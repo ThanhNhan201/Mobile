e62d9d03b1f92b0de9ff93ea275f8b3b4cbc5ad6
ThanhNhan201 <19521929@gm.uit.edu.vn>
2023-05-29 03:03:45 +0700

    hơi lỏ thầy thông cảm xíu nha thầy T_T

--- lab4/app/src/main/java/com/example/myapplication/Login.java
+++ lab4/app/src/main/java/com/example/myapplication/Login.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +24,7 @@ public class Login extends AppCompatActivity {
     private EditText usernameEditText, passwordEditText;
     private TextView signupRedirectText;
     private Button loginButton;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +36,7 @@ public class Login extends AppCompatActivity {
         passwordEditText = findViewById(R.id.password);
         loginButton = findViewById(R.id.btn_login);
         signupRedirectText = findViewById(R.id.registerNow);
-
+        progressBar = findViewById(R.id.progressBar);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,23 +45,27 @@ public class Login extends AppCompatActivity {
                 String password = passwordEditText.getText().toString();
                 String email = username + "@gmail.com";
 
-                if (!username.isEmpty()){
+                if (!username.isEmpty()) {
+                    progressBar.setVisibility(View.VISIBLE);
+
                     auth.signInWithEmailAndPassword(email, password)
                             .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                                 @Override
                                 public void onSuccess(AuthResult authResult) {
-                                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_LONG).show();
+                                    progressBar.setVisibility(View.GONE);
+                                    showCustomToast("Login Successful");
                                     startActivity(new Intent(Login.this, MainActivity.class));
                                     finish();
                                 }
                             }).addOnFailureListener(new OnFailureListener() {
                                 @Override
                                 public void onFailure(@NonNull Exception e) {
-                                    Toast.makeText(Login.this, "Login Failed", Toast.LENGTH_LONG).show();
+                                    progressBar.setVisibility(View.GONE);
+                                    showCustomToast("Login Failed");
                                 }
                             });
                 } else {
-                    usernameEditText.setError("username not valid");
+                    usernameEditText.setError("Username not valid");
                 }
             }
         });
@@ -69,5 +76,20 @@ public class Login extends AppCompatActivity {
                 startActivity(new Intent(Login.this, Register.class));
             }
         });
+    }
+
+    private void showCustomToast(String message) {
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_LONG);
+
+        // Inflate custom layout
+        View customView = getLayoutInflater().inflate(R.layout.custom_toast, null);
+
+        TextView textView = customView.findViewById(R.id.toast_message);
+        textView.setText(message);
+
+        toast.setView(customView);
+        toast.setGravity(Gravity.TOP, 0, 100);
+        toast.show();
     }
 }
